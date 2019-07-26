a2ea9f0314d1593d5c22fbe1441f70b38d8c415f
nsk06 <nonidh.singh@gmail.com>
2019-07-26 14:50:43 +0530

    check

--- app/data/projects.js
+++ app/data/projects.js
@@ -14,7 +14,7 @@ const data = [
     title: 'Video Lecture Slide Mapping',
     subtitle: '93% accuracy',
     link: 'https://github.com/nsk06/Video-Lecture-Slide-Mapper',
-    image: '/images/projects/video-slide.png',
+    image: '/personal-site/images/projects/video-slide.png',
     date: '2019-02-20',
     desc: 'A video lecture slide matching algorithm '
       + 'Use of feature matching(SIFT),Template Matching'
@@ -23,7 +23,7 @@ const data = [
     title: 'PCA EigenFaces Regeneration',
     subtitle: 'Performing face match and regeneration on large dataset using principal component analysis',
     link: 'https://github.com/nsk06/PCA-Face-Regeneration',
-    image: '/images/projects/pca.jpg',
+    image: '/personal-site/images/projects/pca.jpg',
     date: '2019-02-20',
     desc: 'Using PCA on large dataset for feature selection'
       + 'Using eigen faces to compress images '
@@ -32,7 +32,7 @@ const data = [
     title: 'MultiParty Negotiations',
     subtitle: 'A system design and implementation of negotiations among multiple parties based on different strategies and learning',
     link: 'https://github.com/nsk06/MultiParty-Negotiations',
-    image: '/images/projects/multiparty-negotiation.jpg',
+    image: '/personal-site/images/projects/multiparty-negotiation.jpg',
     date: '2019-02-20',
     desc: 'Honours Project Under Prof Praveen Paruchuri'
      + 'Designed and implemented the system for multiparty negotiations'
